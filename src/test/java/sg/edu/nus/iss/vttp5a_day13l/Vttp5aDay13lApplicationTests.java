db51bc54d57a63e7eba0c0778bd3ca50ec1a48e2
remuslum <57741494+remuslum@users.noreply.github.com>
2024-11-20 10:50:12 +0800

    First Commit

---
+++ src/test/java/sg/edu/nus/iss/vttp5a_day13l/Vttp5aDay13lApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_day13l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aDay13lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
